2ece4a3755dcfbcaae7eee1aa3835f9585e3025b
Christoph Siedentop <christophsiedentop@gmail.com>
2017-10-10 17:23:24 +0200

    Address Sanitation for the win: found memory bug.

--- src/detail/NonRandomData.cpp
+++ src/detail/NonRandomData.cpp
@@ -4,7 +4,6 @@
 
 namespace rc {
 RandomData::RandomData(const uint8_t *Data, size_t Size) {
-  std::cout << Data << std::endl;
   // TODO: really stupid way to fill the container
   const size_t size64 = Size / 8;
   auto Data64 = reinterpret_cast<const uint64_t *>(Data);
